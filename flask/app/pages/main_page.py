d45049ad6fb8252964480e2745de78040a502617
Yuri Andriaccio <yurand2000@gmail.com>
2025-01-21 10:50:37 +0100

    Render main page from article

--- flask/app/pages/main_page.py
+++ flask/app/pages/main_page.py
@@ -1,9 +1,14 @@
-from flask import Blueprint
+from flask import Blueprint, abort
 from templates import *
 from globals import *
+from articles import *
 
 main_page_bp = Blueprint("main_page", __name__)
 
 @main_page_bp.route("/")
 def settings():
-    return render_skeleton("Carpa", "")
+    article = render_article(app_config['main-page'], False)
+    if article is None:
+        return abort(404)
+    else:
+        return render_skeleton("Carpa", article['article'])
